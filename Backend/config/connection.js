782066ce253d2fd39825c0211d71f8b39b7afd43
Niko Auer <nikoauer26@gmail.com>
2024-01-04 21:07:13 +1100

    fixed issues with connecting to mongoose db

--- Backend/config/connection.js
+++ Backend/config/connection.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URI)
-        console.log('Connected to Mongo Database 👍🏽')
-    } catch (error) {
-        console.error(err.message);
-        process.exit(1);
-    }
+  try {
+      const connection = await mongoose.connect(process.env.DATABASE_URI);
+
+      console.log('Connected to Mongo Database 👍🏽')
+
+      return connection;
+  } catch (error) {
+      console.error(error.message);
+      process.exit(1);
+  }
 }
+
+export default connectDB;
--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,24 @@ import path from 'path';
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import dotenv from 'dotenv';
-
-
-import connectDB from './config/connection';
+import connectDB from './config/connection.js';
 
 dotenv.config()
 const PORT = process.env.PORT || 3000
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser())
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}!`);
+  });
+}).catch((error) => {
+  console.error('Failed to connect to the database:', error);
+});
+
+app.get("/", (req, res) => {
+  res.send("hello world")
+})
