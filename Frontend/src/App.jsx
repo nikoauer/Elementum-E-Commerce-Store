47cc4c931855c22d207290725397f36ad5b0eb63
Niko Auer <nikoauer26@gmail.com>
2024-01-05 09:20:35 +1100

    added tailwind dependancies and code for tailwind to work

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,10 @@
 
 
 function App() {
-
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+<h1 className="text-3xl font-bold underline decoration-4">
+      Hello world!
+    </h1>
   )
 }
 
