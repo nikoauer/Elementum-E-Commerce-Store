933de2c9c6380ccba6f9dfe26d72728b7f99a690
Niko Auer <nikoauer26@gmail.com>
2024-01-06 14:14:08 +1100

    fixed react-icon import error

--- Frontend/src/pages/Auth/Navigation.jsx
+++ Frontend/src/pages/Auth/Navigation.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
+import {
+    AiOutlineHome,
+    AiOutlineShopping,
+    AiOutlineLogin,
+    AiOutlineUserAdd,
+    AiOutlineShoppingCart,
+  } from "react-icons/ai";
+import { FaHeart } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "./Navigation.css";
+import { useNavigate } from "react-router-dom";
 
 export default function Navigation() {
-    return (
-        <div>Navigation</div>
-    )
+  return (
+    <>
+      <h1>Navigation</h1>
+    </>
+  );
 }
