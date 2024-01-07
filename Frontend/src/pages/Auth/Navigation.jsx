a571594ccad91473c7e34e06f5c7ddd6c2252628
Niko Auer <nikoauer26@gmail.com>
2024-01-07 17:59:22 +1100

    added logo

--- Frontend/src/pages/Auth/Navigation.jsx
+++ Frontend/src/pages/Auth/Navigation.jsx
@@ -10,6 +10,7 @@ import { FaHeart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "./Navigation.css";
 import { useNavigate } from "react-router-dom";
+import logo from "../../images/Logo.svg";
 
 export default function Navigation() {
   const [isNavVisible, setNavVisibility] = useState(false);
@@ -19,33 +20,26 @@ export default function Navigation() {
   };
   return (
     <>
-      <nav class="bg-white border-gray-200 dark:bg-gray-900">
+      <nav class="bg-white border-gray-200 ">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a
-            href="https://flowbite.com/"
-            class="flex items-center space-x-3 rtl:space-x-reverse"
-          >
-            <img
-              src="https://flowbite.com/docs/images/logo.svg"
-              class="h-8"
-              alt="Flowbite Logo"
-            />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+          <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src={logo} class="" alt="Elementum Logo" />
+            <span class="self-center text-2xl font-semibold whitespace-nowrap">
+              Elementum
             </span>
           </a>
           <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <button
               type="button"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
             >
-              Get started
+              Login
             </button>
             <button
               data-collapse-toggle="navbar-cta"
               type="button"
               onClick={toggleNav}
-              class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
               aria-controls="navbar-cta"
               aria-expanded={isNavVisible}
             >
@@ -68,14 +62,16 @@ export default function Navigation() {
             </button>
           </div>
           <div
-             className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isNavVisible ? '' : 'hidden'}`}
+            className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
+              isNavVisible ? "" : "hidden"
+            }`}
             id="navbar-cta"
           >
-            <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
               <li>
                 <a
                   href="#"
-                  class="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
+                  class="block py-2 px-3 md:p-0 text-black rounded hover:bg-green-500 md:hover:bg-transparent md:hover:text-blue-700"
                   aria-current="page"
                 >
                   Home
@@ -84,25 +80,25 @@ export default function Navigation() {
               <li>
                 <a
                   href="#"
-                  class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  class="block py-2 px-3 md:p-0 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700"
                 >
-                  About
+                  Shop
                 </a>
               </li>
               <li>
                 <a
                   href="#"
-                  class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  class="block py-2 px-3 md:p-0 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 "
                 >
-                  Services
+                  Cart
                 </a>
               </li>
               <li>
                 <a
                   href="#"
-                  class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  class="block py-2 px-3 md:p-0 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700"
                 >
-                  Contact
+                  Favourites
                 </a>
               </li>
             </ul>
