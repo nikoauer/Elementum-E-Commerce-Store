106303e9130246001e48c35b116f8c3a4bf5b7c3
Niko Auer <nikoauer26@gmail.com>
2024-01-08 21:17:48 +1100

    added registry button

--- Frontend/src/pages/Auth/Navigation.jsx
+++ Frontend/src/pages/Auth/Navigation.jsx
@@ -20,31 +20,38 @@ export default function Navigation() {
   };
   return (
     <>
-      <nav id="" class="bg-white border-gray-200 ">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <Link to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src={logo} class="h-14" alt="Elementum Logo" />
-            <span class="self-center text-2xl font-semibold whitespace-nowrap">
+      <nav id="" className="bg-white border-gray-200 ">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <img src={logo} className="h-14" alt="Elementum Logo" />
+            <span className="self-center text-2xl font-semibold whitespace-nowrap">
               ELEMENTUM
             </span>
           </Link>
-          <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <div className="space-x-2">
             <button
               type="button"
-              class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
             > <Link to="/login">Login</Link>
             </button>
+            <button
+              type="button"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
+            > <Link to="/register">Register</Link>
+            </button>
+            </div>
             <button
               data-collapse-toggle="navbar-cta"
               type="button"
               onClick={toggleNav}
-              class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-700 rounded-lg md:hidden hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-200"
+              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-700 rounded-lg md:hidden hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-200"
               aria-controls="navbar-cta"
               aria-expanded={isNavVisible}
             >
-              <span class="sr-only">Open main menu</span>
+              <span className="sr-only">Open main menu</span>
               <svg
-                class="w-5 h-5"
+                className="w-5 h-5"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
@@ -52,9 +59,9 @@ export default function Navigation() {
               >
                 <path
                   stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
                   d="M1 1h15M1 7h15M1 13h15"
                 />
               </svg>
@@ -66,11 +73,11 @@ export default function Navigation() {
             }`}
             id="navbar-cta"
           >
-            <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-50 rounded-lg bg-gray-100 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
+            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-50 rounded-lg bg-gray-100 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
               <li>
                 <Link
                 to="/"
-                  class="block py-2 px-3 md:p-0 text-black rounded font-semibold hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
+                  className="block py-2 px-3 md:p-0 text-black rounded font-semibold hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
                   aria-current="page"
                 >
                   Home
@@ -79,7 +86,7 @@ export default function Navigation() {
               <li>
                 <Link
                 to="/shop"
-                  class="block py-2 px-3 md:p-0 text-black rounded font-semibold hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
+                  className="block py-2 px-3 md:p-0 text-black rounded font-semibold hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
                 >
                   Shop
                 </Link>
@@ -87,7 +94,7 @@ export default function Navigation() {
               <li>
                 <Link
                 to="/cart"
-                  class="block py-2 px-3 md:p-0 text-black rounded font-semibold hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
+                  className="block py-2 px-3 md:p-0 text-black rounded font-semibold hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
                 >
                   Cart
                 </Link>
@@ -95,7 +102,7 @@ export default function Navigation() {
               <li>
                 <Link
                 to="/favourites"
-                  class="block py-2 px-3 md:p-0 text-black rounded font-semibold hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
+                  className="block py-2 px-3 md:p-0 text-black rounded font-semibold hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
                 >
                   Favourites
                 </Link>
