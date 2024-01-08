9cd2317b2391005f9097c4382eb0f337f30584c1
Niko Auer <nikoauer26@gmail.com>
2024-01-08 21:10:07 +1100

    added links to anchors

--- Frontend/src/pages/Auth/Navigation.jsx
+++ Frontend/src/pages/Auth/Navigation.jsx
@@ -22,18 +22,17 @@ export default function Navigation() {
     <>
       <nav id="" class="bg-white border-gray-200 ">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
+          <Link to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
             <img src={logo} class="h-14" alt="Elementum Logo" />
             <span class="self-center text-2xl font-semibold whitespace-nowrap">
               ELEMENTUM
             </span>
-          </a>
+          </Link>
           <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
             <button
               type="button"
               class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
-            >
-              Login
+            > <Link to="/login">Login</Link>
             </button>
             <button
               data-collapse-toggle="navbar-cta"
@@ -69,38 +68,37 @@ export default function Navigation() {
           >
             <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-50 rounded-lg bg-gray-100 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
               <li>
-                <a
-                  href="#"
-                  class="block py-2 px-3 md:p-0 text-black rounded hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
+                <Link
+                to="/"
+                  class="block py-2 px-3 md:p-0 text-black rounded font-semibold hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
                   aria-current="page"
                 >
                   Home
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
-                  class="block py-2 px-3 md:p-0 text-black rounded hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
+                <Link
+                to="/shop"
+                  class="block py-2 px-3 md:p-0 text-black rounded font-semibold hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
                 >
                   Shop
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#"
-                  class="block py-2 px-3 md:p-0 text-black rounded hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-700 "
+                <Link
+                to="/cart"
+                  class="block py-2 px-3 md:p-0 text-black rounded font-semibold hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
                 >
                   Cart
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                id="favourites"
-                  href="#"
-                  class="block py-2 px-3 md:p-0 text-black rounded hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-700"
+                <Link
+                to="/favourites"
+                  class="block py-2 px-3 md:p-0 text-black rounded font-semibold hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
                 >
                   Favourites
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
