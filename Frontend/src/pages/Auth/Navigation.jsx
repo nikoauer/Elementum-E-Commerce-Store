f15813a314ca9ceb3d54000f4d5eff42e7d3b2c9
Niko Auer <nikoauer26@gmail.com>
2024-01-08 20:48:34 +1100

    minor tailwind modifications to navbar

--- Frontend/src/pages/Auth/Navigation.jsx
+++ Frontend/src/pages/Auth/Navigation.jsx
@@ -20,12 +20,12 @@ export default function Navigation() {
   };
   return (
     <>
-      <nav class="bg-white border-gray-200 ">
+      <nav id="" class="bg-white border-gray-200 ">
         <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src={logo} class="h-20" alt="Elementum Logo" />
+            <img src={logo} class="h-14" alt="Elementum Logo" />
             <span class="self-center text-2xl font-semibold whitespace-nowrap">
-              Elementum
+              ELEMENTUM
             </span>
           </a>
           <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
@@ -39,7 +39,7 @@ export default function Navigation() {
               data-collapse-toggle="navbar-cta"
               type="button"
               onClick={toggleNav}
-              class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
+              class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-700 rounded-lg md:hidden hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-200"
               aria-controls="navbar-cta"
               aria-expanded={isNavVisible}
             >
@@ -67,11 +67,11 @@ export default function Navigation() {
             }`}
             id="navbar-cta"
           >
-            <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
+            <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-50 rounded-lg bg-gray-100 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
               <li>
                 <a
                   href="#"
-                  class="block py-2 px-3 md:p-0 text-black rounded hover:bg-green-500 md:hover:bg-transparent md:hover:text-blue-700"
+                  class="block py-2 px-3 md:p-0 text-black rounded hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
                   aria-current="page"
                 >
                   Home
@@ -80,7 +80,7 @@ export default function Navigation() {
               <li>
                 <a
                   href="#"
-                  class="block py-2 px-3 md:p-0 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700"
+                  class="block py-2 px-3 md:p-0 text-black rounded hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
                 >
                   Shop
                 </a>
@@ -88,15 +88,16 @@ export default function Navigation() {
               <li>
                 <a
                   href="#"
-                  class="block py-2 px-3 md:p-0 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 "
+                  class="block py-2 px-3 md:p-0 text-black rounded hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-700 "
                 >
                   Cart
                 </a>
               </li>
               <li>
                 <a
+                id="favourites"
                   href="#"
-                  class="block py-2 px-3 md:p-0 text-black rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700"
+                  class="block py-2 px-3 md:p-0 text-black rounded hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-700"
                 >
                   Favourites
                 </a>
