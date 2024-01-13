658ae0adbba3dc2e93b599254fc2b414f800dfeb
Niko Auer <nikoauer26@gmail.com>
2024-01-13 15:37:52 +1100

    finished navbar

--- Frontend/src/pages/Auth/Navigation.jsx
+++ Frontend/src/pages/Auth/Navigation.jsx
@@ -1,19 +1,26 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navigation.css";
-import logo from "../../images/logo5.svg";
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { Bars3Icon, BellIcon, XMarkIcon, HeartIcon, ShoppingCartIcon } from '@heroicons/react/24/outline'
-import { PlusIcon } from '@heroicons/react/20/solid'
+import logo from "../../images/logo8.svg";
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import {
+  Bars3Icon,
+  XMarkIcon,
+  HeartIcon,
+  ShoppingCartIcon,
+  BuildingStorefrontIcon,
+  HomeIcon
+} from "@heroicons/react/24/outline";
+import { PlusIcon } from "@heroicons/react/20/solid";
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function Navigation() {
   return (
-    <Disclosure as="nav" className="bg-gray-50 shadow">
+    <Disclosure as="nav" className="bg-white shadow">
       {({ open }) => (
         <>
           <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
@@ -21,7 +28,7 @@ export default function Navigation() {
               <div className="flex">
                 <div className="-ml-2 mr-2 flex items-center md:hidden">
                   {/* Mobile menu button */}
-                  <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-sky-500">
+                  <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-200 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-sky-500">
                     <span className="absolute -inset-0.5" />
                     <span className="sr-only">Open main menu</span>
                     {open ? (
@@ -32,15 +39,11 @@ export default function Navigation() {
                   </Disclosure.Button>
                 </div>
                 <div className="flex flex-shrink-0 items-center">
-                  <img
-                    className="h-12 w-auto"
-                    src={logo}
-                    alt="Your Company"
-                  /> 
+                  <img className="h-12 w-auto" src={logo} alt="Your Company" />
                 </div>
                 <div className="hidden md:ml-6 md:flex md:space-x-8">
                   <Link
-                   to="/"
+                    to="/"
                     className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
                   >
                     Home
@@ -57,32 +60,37 @@ export default function Navigation() {
                 <div className="hidden md:ml-4 md:flex md:flex-shrink-0 md:items-center">
                   <button
                     type="button"
-                    className="relative rounded-full bg-gray-50 p-1 text-gray-500 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
-                  ><Link to="/favourites">
-                    <HeartIcon className="h-6 w-6" aria-hidden="true" />
-                  </Link>
+                    className="relative rounded-full bg-white p-1 text-gray-500 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
+                  >
+                    <Link to="/favourites">
+                      <HeartIcon className="h-6 w-6" aria-hidden="true" />
+                    </Link>
                   </button>
                   <button
                     type="button"
-                    className="mx-2 relative rounded-full bg-gray-50 p-1 text-gray-500 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
-                  ><Link to="/cart">
-                    <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
-                  </Link>
+                    className="mx-2 relative rounded-full bg-white p-1 text-gray-500 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
+                  >
+                    <Link to="/cart">
+                      <ShoppingCartIcon
+                        className="h-6 w-6"
+                        aria-hidden="true"
+                      />
+                    </Link>
                   </button>
                   <div className="flex-shrink-0">
-                  <button
-                    type="button"
-                    className="mx-2 relative inline-flex items-center gap-x-1.5 rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    <Link to="/signup">Signup</Link>
-                  </button>
-                  <button
-                    type="button"
-                    className="relative inline-flex items-center gap-x-1.5 rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  >
-                    <Link to="/login">Login</Link>
-                  </button>
-                </div>
+                    <button
+                      type="button"
+                      className="mx-2 relative inline-flex items-center gap-x-1.5 rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      <Link to="/signup">Sign Up</Link>
+                    </button>
+                    <button
+                      type="button"
+                      className="relative inline-flex items-center gap-x-1.5 rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    >
+                      <Link to="/login">Login</Link>
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
@@ -92,59 +100,68 @@ export default function Navigation() {
             <div className="space-y-1 pb-3 pt-2">
               <Disclosure.Button
                 as="a"
-                className="block border-l-4 border-sky-500 bg-sky-100 py-2 pl-3 pr-4 text-base font-medium text-blue-700 sm:pl-5 sm:pr-6"
-              ><Link to="/">
-                Home
-              </Link>
+                className="flex items-center border-l-4 border-sky-500 bg-sky-100 py-2 pl-3 pr-4 text-base font-medium text-blue-700 sm:pl-5 sm:pr-6"
+              >
+                <Link to="/" className="flex items-center">
+                    <HomeIcon className="h-6 w-6" />
+                    <span className="ml-2">Home</span>
+                </Link>
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
-              ><Link to="/shop">
-                Shop
-              </Link>
+                className="flex items-center border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+              >
+                <Link to="/shop" className="flex items-center">
+                  <BuildingStorefrontIcon className="h-6 w-6" />
+                  <span className="ml-2">Shop</span>
+                </Link>
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
-              ><Link to="/favourites">
-                Favourites
-              </Link>
+                className="flex items-center border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+              >
+                <Link to="/favourites" className="flex items-center">
+                  <HeartIcon className="h-6 w-6"/>
+                    <span className="ml-2">Favourites</span>
+                </Link>
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
-                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
-              ><Link to="/cart">
-                <span><ShoppingCartIcon className="h-6 w-6">Cart</ShoppingCartIcon> Cart</span>
-              </Link>
+                className="flex items-center border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+              >
+                <Link to="/cart" className="flex items-center">
+                  <ShoppingCartIcon className="h-6 w-6" />
+                  <span className="ml-2">Cart</span>
+                </Link>
               </Disclosure.Button>
+
               <div className="relative">
-      <div className="absolute inset-0 flex items-center" aria-hidden="true">
-        <div className="w-full border-t border-gray-300" />
-      </div>
-      <div className="relative flex justify-center">
-        <span className="bg-white px-2 text-gray-500">
-        </span>
-      </div>
-    </div>
+                <div
+                  className="absolute inset-0 flex items-center"
+                  aria-hidden="true"
+                >
+                  <div className="w-full border-t border-gray-300" />
+                </div>
+                <div className="relative flex justify-center">
+                  <span className="bg-white px-2 text-gray-500"></span>
+                </div>
+              </div>
               <Disclosure.Button
                 as="a"
                 className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
-              ><Link to="/login">
-                Login
-              </Link>
+              >
+                <Link to="/login">Login</Link>
               </Disclosure.Button>
               <Disclosure.Button
                 as="a"
                 className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
-              ><Link to="/signup">
-                Signup
-              </Link>
+              >
+                <Link to="/signup">Sign Up</Link>
               </Disclosure.Button>
             </div>
           </Disclosure.Panel>
         </>
       )}
     </Disclosure>
-  )
+  );
 }
