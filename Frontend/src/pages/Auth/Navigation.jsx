39d7c0e559bdd3d6f157268f6ac70af2e43bcf39
Niko Auer <nikoauer26@gmail.com>
2024-01-13 15:04:06 +1100

    remade nav bar with tailwind ui component

--- Frontend/src/pages/Auth/Navigation.jsx
+++ Frontend/src/pages/Auth/Navigation.jsx
@@ -1,116 +1,150 @@
 import { useState } from "react";
-import {
-  AiOutlineHome,
-  AiOutlineShopping,
-  AiOutlineLogin,
-  AiOutlineUserAdd,
-  AiOutlineShoppingCart,
-} from "react-icons/ai";
-import { FaHeart } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import "./Navigation.css";
-import { useNavigate } from "react-router-dom";
-import logo from "../../images/Logo3.svg";
+import logo from "../../images/logo5.svg";
+import { Fragment } from 'react'
+import { Disclosure, Menu, Transition } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon, HeartIcon, ShoppingCartIcon } from '@heroicons/react/24/outline'
+import { PlusIcon } from '@heroicons/react/20/solid'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 export default function Navigation() {
-  const [isNavVisible, setNavVisibility] = useState(false);
-
-  const toggleNav = () => {
-    setNavVisibility(!isNavVisible);
-  };
   return (
-    <>
-      <nav id="" className="bg-white border-gray-200 ">
-        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-          <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-            <img src={logo} className="h-14" alt="Elementum Logo" />
-            <span className="self-center text-2xl font-semibold whitespace-nowrap">
-              ELEMENTUM
-            </span>
-          </Link>
-          <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <div className="space-x-2">
-            <button
-              type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
-            > <Link to="/login">Login</Link>
-            </button>
-            <button
-              type="button"
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"
-            > <Link to="/register">Register</Link>
-            </button>
+    <Disclosure as="nav" className="bg-gray-50 shadow">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="flex h-16 justify-between">
+              <div className="flex">
+                <div className="-ml-2 mr-2 flex items-center md:hidden">
+                  {/* Mobile menu button */}
+                  <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-sky-500">
+                    <span className="absolute -inset-0.5" />
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
+                </div>
+                <div className="flex flex-shrink-0 items-center">
+                  <img
+                    className="h-12 w-auto"
+                    src={logo}
+                    alt="Your Company"
+                  /> 
+                </div>
+                <div className="hidden md:ml-6 md:flex md:space-x-8">
+                  <Link
+                   to="/"
+                    className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                  >
+                    Home
+                  </Link>
+                  <Link
+                    to="/shop"
+                    className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
+                  >
+                    Shop
+                  </Link>
+                </div>
+              </div>
+              <div className="flex items-center">
+                <div className="hidden md:ml-4 md:flex md:flex-shrink-0 md:items-center">
+                  <button
+                    type="button"
+                    className="relative rounded-full bg-gray-50 p-1 text-gray-500 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
+                  ><Link to="/favourites">
+                    <HeartIcon className="h-6 w-6" aria-hidden="true" />
+                  </Link>
+                  </button>
+                  <button
+                    type="button"
+                    className="mx-2 relative rounded-full bg-gray-50 p-1 text-gray-500 hover:text-gray-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
+                  ><Link to="/cart">
+                    <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
+                  </Link>
+                  </button>
+                  <div className="flex-shrink-0">
+                  <button
+                    type="button"
+                    className="mx-2 relative inline-flex items-center gap-x-1.5 rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    <Link to="/signup">Signup</Link>
+                  </button>
+                  <button
+                    type="button"
+                    className="relative inline-flex items-center gap-x-1.5 rounded-md bg-sky-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-sky-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  >
+                    <Link to="/login">Login</Link>
+                  </button>
+                </div>
+                </div>
+              </div>
             </div>
-            <button
-              data-collapse-toggle="navbar-cta"
-              type="button"
-              onClick={toggleNav}
-              className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-700 rounded-lg md:hidden hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-gray-200"
-              aria-controls="navbar-cta"
-              aria-expanded={isNavVisible}
-            >
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
-            </button>
           </div>
-          <div
-            className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
-              isNavVisible ? "" : "hidden"
-            }`}
-            id="navbar-cta"
-          >
-            <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-50 rounded-lg bg-gray-100 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white">
-              <li>
-                <Link
-                to="/"
-                  className="block py-2 px-3 md:p-0 text-black rounded font-semibold hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
-                  aria-current="page"
-                >
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link
-                to="/shop"
-                  className="block py-2 px-3 md:p-0 text-black rounded font-semibold hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
-                >
-                  Shop
-                </Link>
-              </li>
-              <li>
-                <Link
-                to="/cart"
-                  className="block py-2 px-3 md:p-0 text-black rounded font-semibold hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
-                >
-                  Cart
-                </Link>
-              </li>
-              <li>
-                <Link
-                to="/favourites"
-                  className="block py-2 px-3 md:p-0 text-black rounded font-semibold hover:bg-blue-200 md:hover:bg-transparent md:hover:text-blue-600"
-                >
-                  Favourites
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-    </>
-  );
+
+          <Disclosure.Panel className="md:hidden">
+            <div className="space-y-1 pb-3 pt-2">
+              <Disclosure.Button
+                as="a"
+                className="block border-l-4 border-sky-500 bg-sky-100 py-2 pl-3 pr-4 text-base font-medium text-blue-700 sm:pl-5 sm:pr-6"
+              ><Link to="/">
+                Home
+              </Link>
+              </Disclosure.Button>
+              <Disclosure.Button
+                as="a"
+                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+              ><Link to="/shop">
+                Shop
+              </Link>
+              </Disclosure.Button>
+              <Disclosure.Button
+                as="a"
+                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+              ><Link to="/favourites">
+                Favourites
+              </Link>
+              </Disclosure.Button>
+              <Disclosure.Button
+                as="a"
+                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+              ><Link to="/cart">
+                <span><ShoppingCartIcon className="h-6 w-6">Cart</ShoppingCartIcon> Cart</span>
+              </Link>
+              </Disclosure.Button>
+              <div className="relative">
+      <div className="absolute inset-0 flex items-center" aria-hidden="true">
+        <div className="w-full border-t border-gray-300" />
+      </div>
+      <div className="relative flex justify-center">
+        <span className="bg-white px-2 text-gray-500">
+        </span>
+      </div>
+    </div>
+              <Disclosure.Button
+                as="a"
+                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+              ><Link to="/login">
+                Login
+              </Link>
+              </Disclosure.Button>
+              <Disclosure.Button
+                as="a"
+                className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-700 sm:pl-5 sm:pr-6"
+              ><Link to="/signup">
+                Signup
+              </Link>
+              </Disclosure.Button>
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
 }
