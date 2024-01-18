dca2a3a978db1d7b9b4124a412284021498166c3
Niko Auer <nikoauer26@gmail.com>
2024-01-18 21:41:33 +1100

    added user list route

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -14,19 +14,24 @@ import Register from './pages/Auth/Register.jsx'
 //Private directories
 import PrivateRoute from './components/privateRoutes.jsx';
 import Profile from './pages/User/Profile.jsx';
-
+import AdminRoutes from './pages/Admin/AdminRoutes.jsx';
+import UserList from './pages/Admin/UserList.jsx';
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App />}>
+      <Route path='/login' element={<Login />} />
+      <Route path='/signup' element={<Register />} />
 
       <Route path="" element={<PrivateRoute />}>
         <Route path="/profile" element={<Profile />} />
       </Route>
 
-      <Route path='/login' element={<Login />} />
-      <Route path='/signup' element={<Register />} />
+    {/* admin routes */}
+    <Route path='/admin' element={<AdminRoutes/>}>
+      <Route path='userlist' element={<UserList/>} />
     </Route>
+  </Route>
   )
 )
 
