dee407c71d48a0fa327b589611f3261748883249
Niko Auer <nikoauer26@gmail.com>
2024-01-18 22:39:38 +1100

    created user table in userlist

--- Frontend/src/pages/Admin/UserList.jsx
+++ Frontend/src/pages/Admin/UserList.jsx
@@ -2,13 +2,90 @@ import { useEffect, useState } from "react"
 import {  HomeIcon } from "@heroicons/react/24/outline";
 import { toast } from "react-toastify";
 import Loader from "../../components/Loader";
+import {   useGetUserQuery,
+    useDeleteUserMutation,
+    useUpdateUserMutation} from "../../redux/api/usersApiSlice"
+import Message from "../../components/Message";
 
 
 
 const UserList = () => {
+    const { data: users, refetch, isLoading, error } = useGetUserQuery()
+    const [deleteUser] = useDeleteUserMutation()
+    const [updateUser] = useUpdateUserMutation()
+
+    const [editUserId, seteditUserId] = useState(null)
+    const [editUsername, setUsername] = useState('')
+    const [editUserEmail, setUserEmail] = useState('')
+
+    useEffect(() => {
+        refetch();
+    }, [refetch]);
+
+    const people = [
+        { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
+        // More people...
+      ]
 
     return (
-       <h1>UserList</h1>
+        <div className="px-4 sm:px-6 lg:px-8 ">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-base font-semibold leading-6 text-gray-900">Users</h1>
+          <p className="mt-2 text-sm text-gray-700">
+            A list of all the users in your account including their id, name, email and whether they are an administrator.
+          </p>
+        </div>
+      </div>
+      {isLoading ? ( <Loader/>) : error ? (<Message variant="danger">
+          {error?.data?.message || error.error}
+        </Message>) : (
+            <div className="mt-8 flow-root">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead>
+                <tr>
+                  <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">
+                    NAME
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    ID
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    EMAIL
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                    ADMINISTRATOR
+                  </th>
+                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-3">
+                    <span className="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white">
+                {users.map((user) => (
+                  <tr key={user._id} className="even:bg-gray-50">
+                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-800 sm:pl-3">
+                      {user.username}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{user._id}</td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{user.email}</td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{user.isAdmin}</td>
+                    <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
+                      <a href="#" className="text-red-600 hover:text-red-800">
+                        Delete<span className="sr-only">, {user.name}</span>
+                      </a>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+        )}
+    </div>
     )
 }
 
