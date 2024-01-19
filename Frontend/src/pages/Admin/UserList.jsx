6412fd15b6e4092e80d065ab06ad4b1257e03226
Niko Auer <nikoauer26@gmail.com>
2024-01-19 11:24:31 +1100

    fixed eddit toggle with usernames

--- Frontend/src/pages/Admin/UserList.jsx
+++ Frontend/src/pages/Admin/UserList.jsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react"
-import {  HomeIcon } from "@heroicons/react/24/outline";
+import {  CheckCircleIcon, XCircleIcon } from "@heroicons/react/24/outline";
 import { toast } from "react-toastify";
 import Loader from "../../components/Loader";
 import {   useGetUserQuery,
     useDeleteUserMutation,
-    useUpdateUserMutation} from "../../redux/api/usersApiSlice"
+    useUpdateUserMutation} from "../../redux/api/usersAPISlice"
 import Message from "../../components/Message";
+import { FaRegEdit, FaCheck, FaTimes } from 'react-icons/fa';
+
 
 
 
@@ -15,17 +17,43 @@ const UserList = () => {
     const [updateUser] = useUpdateUserMutation()
 
     const [editUserId, seteditUserId] = useState(null)
-    const [editUsername, setUsername] = useState('')
-    const [editUserEmail, setUserEmail] = useState('')
+    const [editUsername, setEditUsername] = useState('')
+    const [editUserEmail, setEditUserEmail] = useState('')
 
     useEffect(() => {
         refetch();
     }, [refetch]);
 
-    const people = [
-        { name: 'Lindsay Walton', title: 'Front-end Developer', email: 'lindsay.walton@example.com', role: 'Member' },
-        // More people...
-      ]
+    const toggleEdit = (id, username, email) => {
+        seteditUserId(id);
+        setEditUsername(username);
+        setEditUserEmail(email);
+      };
+
+      const deleteHandler = async (id) => {
+        if (window.confirm("Are you sure?")) {
+          try {
+            await deleteUser(id);
+            refetch();
+          } catch (err) {
+            toast.error(err?.data?.message || err.error);
+          }
+        }
+      };
+
+      const updateHandler = async (id) => {
+        try {
+          await updateUser({
+            userId: id,
+            username: editUsername,
+            email: editUserEmail,
+          });
+          seteditUserId(null);
+          refetch();
+        } catch (err) {
+          toast.error(err?.data?.message || err.error);
+        }
+      };
 
     return (
         <div className="px-4 sm:px-6 lg:px-8 ">
@@ -33,7 +61,7 @@ const UserList = () => {
         <div className="sm:flex-auto">
           <h1 className="text-base font-semibold leading-6 text-gray-900">Users</h1>
           <p className="mt-2 text-sm text-gray-700">
-            A list of all the users in your account including their id, name, email and whether they are an administrator.
+            A list of all the users that have accounts currently on Elementum. It shows their id, name, email and whether they are an administrator. You can edit the users email, username or delete the user.
           </p>
         </div>
       </div>
@@ -47,10 +75,10 @@ const UserList = () => {
               <thead>
                 <tr>
                   <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">
-                    NAME
+                    ID
                   </th>
                   <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
-                    ID
+                    NAME
                   </th>
                   <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
                     EMAIL
@@ -67,11 +95,43 @@ const UserList = () => {
                 {users.map((user) => (
                   <tr key={user._id} className="even:bg-gray-50">
                     <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-800 sm:pl-3">
-                      {user.username}
+                      {user._id}
                     </td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{user._id}</td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{user.email}</td>
-                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{user.isAdmin}</td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-700">
+                    {editUserId === user._id ? (
+                        <div>
+                        <input
+                        className="rounded p-1"
+                          type="text"
+                          value={editUsername}
+                          onChange={(e) => setEditUsername(e.target.value)}
+                        />
+                        <button onClick={() => updateHandler(user._id)}>
+                        <FaCheck className="text-green-600 mx-1.5"/>
+                    </button>
+                    <button onClick={() => seteditUserId(null)}>
+                        <FaTimes className="text-red-600"/>
+                    </button>
+                        </div>
+                    ) : (
+                        <div>
+                            {user.username}{" "}
+                        <button
+                          onClick={() =>
+                            toggleEdit(user._id, user.username, user.email)
+                          }
+                        >
+                          <FaRegEdit className="mx-1 hover:text-blue-600" />
+                        </button>
+                        </div>
+                    )}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-700">{user.email}</td>
+                    <td className="whitespace-nowrap px-3 py-4 flex justify-center">{user.isAdmin ? (
+                        <CheckCircleIcon className="h-6 text-green-600"></CheckCircleIcon>
+                    ) : (
+                        <XCircleIcon className="h-6 text-red-600"></XCircleIcon>
+                    )}</td>
                     <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-3">
                       <a href="#" className="text-red-600 hover:text-red-800">
                         Delete<span className="sr-only">, {user.name}</span>
