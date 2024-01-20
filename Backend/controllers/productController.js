829ca2ca4a529918be574d8219c4cf4eb5ac9505
Niko Auer <nikoauer26@gmail.com>
2024-01-20 20:37:09 +1100

    update product function working

--- Backend/controllers/productController.js
+++ Backend/controllers/productController.js
@@ -33,4 +33,37 @@ const addProduct = asyncHandler(async (req, res) => {
     }
   });
 
-export { addProduct };
+  const updateProductDetails = asyncHandler(async(req, res) => {
+    try {
+        const { brand, name, description, price, category, quantity, image } = req.body;
+  
+      // Validation
+      switch (true) {
+        case !name:
+          return res.json({ error: "Name is required" });
+        case !brand:
+          return res.json({ error: "Brand is required" });
+        case !description:
+          return res.json({ error: "Description is required" });
+        case !price:
+          return res.json({ error: "Price is required" });
+        case !category:
+          return res.json({ error: "Category is required" });
+        case !quantity:
+          return res.json({ error: "Quantity is required" });
+          case !image:
+          return res.json({ error: "Image is required" })
+      }
+      const updatedProduct = await Product.findByIdAndUpdate(req.params.id, {brand, name, description, price, category, quantity, image}, {new: true} )
+      
+      await updatedProduct.save()
+      
+      res.json(updatedProduct)
+        
+    } catch (error) {
+        console.error(error);
+        res.status(400).json(error.message);   
+    }
+  })
+
+export { addProduct, updateProductDetails };
--- Backend/routes/productRoutes.js
+++ Backend/routes/productRoutes.js
@@ -6,9 +6,12 @@ import { authAdmin, authenticateUser } from "../middlewares/authMiddleware.js";
 import checkID from "../middlewares/checkId.js";
 
 import {
-    addProduct
+    addProduct,
+    updateProductDetails
 } from "../controllers/productController.js"
 
 router.route('/').post(authenticateUser, authAdmin, addProduct);
+
+router.route('/:id').put(authenticateUser, authAdmin, updateProductDetails)
 
 export default router;
