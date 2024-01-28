addae37df2715db150b6ca8643003a3989739991
Niko Auer <nikoauer26@gmail.com>
2024-01-28 19:03:14 +1100

    home page has banner and carousel working

--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -3,6 +3,8 @@ import { useGetProductsQuery } from "./redux/api/productAPISlice";
 import Message from "./components/Message";
 import Loader from "./components/Loader";
 import Carousel from "./components/Carousel";
+import Banner from "./images/Banner.png"
+
 
 const Home = () => {
   const { keyword } = useParams();
@@ -17,9 +19,14 @@ const Home = () => {
         </Message>
       ) : (
         <>
+        <div className="flex justify-center">
+            <img src={Banner} alt="Be in your element" className="min-w-full" />
+        </div>
           <div className="flex items-center justify-center mt-1">
             <div className="px-4 sm:px-6 lg:px-8 w-full max-w-screen-xl">
-                <Carousel />
+                <div className="pt-5">
+                 <Carousel />
+                </div>
             </div>
           </div>
         </>
--- Frontend/src/components/Carousel.jsx
+++ Frontend/src/components/Carousel.jsx
@@ -29,4 +29,5 @@ const Carousel = () => {
   )
 }
 
+
 export default Carousel
