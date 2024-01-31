ae05b66bde68b0c281e12256ee3a3f93efcef1c2
Niko Auer <nikoauer26@gmail.com>
2024-01-31 12:03:11 +1100

    re-styled the home banner

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -5,6 +5,7 @@ import Loader from "../components/Loader";
 import Carousel from "../components/Carousel";
 import Banner from "../images/Banner.png";
 import Product from "./Products/Product";
+import { FaArrowRight } from "react-icons/fa";
 
 const Home = () => {
   const { keyword } = useParams();
@@ -19,9 +20,21 @@ const Home = () => {
         </Message>
       ) : (
         <>
-          <div className="flex justify-center">
-            <img src={Banner} alt="Be in your element" className="min-w-full" />
+          <div className="relative">
+            <div className="relative">
+              <img src={Banner} alt="Be in your element" className="min-w-full" />
+              <div className="absolute xl:left-10 md:left-10 sm:left-5 top-1/2 transform -translate-y-1/2 sm:w-1/3 md:w-1/3 lg:w-1/3">
+                <h1 className="text-white xl:text-8xl md:text-5xl sm:text-3xl font-bold mb-2">
+                  Don't let the elements stop the adventure
+                </h1>
+                  <Link to="/shop" className="inline-flex items-center bg-sky-500 hover:bg-sky-600 text-black hover:text-white px-2 sm:px-4 py-1 sm:py-2 rounded text-sm sm:text-base">
+                    <span>Shop the latest gear</span>
+                    <FaArrowRight className="ml-2" />
+                  </Link>
+              </div>
+            </div>
           </div>
+
           <div className="flex items-center justify-center mt-1">
             <div className="px-4 sm:px-6 lg:px-8 w-full max-w-screen-xl">
               <div className="pt-5 pb-5">
