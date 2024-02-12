0732932855239b67bc5fe2d4d10fff5b52043c99
Niko Auer <nikoauer26@gmail.com>
2024-02-12 21:36:59 +1100

    added address usestates in shipping.jsx

--- Frontend/src/pages/Orders/Shipping.jsx
+++ Frontend/src/pages/Orders/Shipping.jsx
@@ -1,6 +1,31 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import { saveShippingAddress, savePaymentMethod } from '../../redux/features/cart/cartSlice'
+
 
 const Shipping = () => {
+
+const [paypaylMethod, setPaypalMethod] = useState('Paypal')
+const [address, setAddress] = useState(shippingAddress.address || '')
+const [city, setCity] = useState(shippingAddress.city || '')
+const [postalCode, setPostalCode] = useState(shippingAddress.postalCode || '')
+const [state, setState] = useState(shippingAddress.state || '')
+const [country, setCountry] = useState(shippingAddress.country || '')
+
+const cart = useSelector((state) => state.cart)
+const {shippingAddress} = cart
+
+const dispatch = useDispatch()
+const navigate = useNavigate()
+
+useEffect(() => {
+    if(!shippingAddress.address) {
+        navigate('/shipping')
+    }
+}, [navigate, shippingAddress])
+
   return (
     <div>Shipping</div>
   )
