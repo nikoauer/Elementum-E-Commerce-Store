a43d05876516cabb309286a5c3bc077b0c8d2902
Niko Auer <nikoauer26@gmail.com>
2024-02-17 14:39:03 +1100

    paypal buttons are appearing in order.jsx

--- Frontend/src/pages/Orders/Order.jsx
+++ Frontend/src/pages/Orders/Order.jsx
@@ -5,7 +5,7 @@ import { toast } from "react-toastify";
 import { useSelector } from "react-redux";
 import Message from "../../components/Message";
 import Loader from "../../components/Loader";
-import logo from "../../images/logo8.svg"
+import logo from "../../images/logo8.svg";
 import {
   useDeliverOrderMutation,
   useGetOrderDetailsQuery,
@@ -22,7 +22,9 @@ const Order = () => {
     error,
   } = useGetOrderDetailsQuery(orderId);
 
-  const [payOrder, { isLoading: loadingDeliver }] = useDeliverOrderMutation();
+  const [payOrder, { isLoading: loadingPay }] = usePayOrderMutation();
+  const [deliveryOrder, { isLoading: loadingDeliver }] =
+    useDeliverOrderMutation();
   const { userInfo } = useSelector((state) => state.auth);
 
   const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
@@ -59,153 +61,183 @@ const Order = () => {
     <Message variant="danger">{error.data.message}</Message>
   ) : (
     <div className="flex items-center justify-center mt-1">
-    <div className="px-4 sm:px-6 lg:px-8 w-full max-w-screen-xl">
+      <div className="px-4 sm:px-6 lg:px-8 w-full max-w-screen-xl">
         {order.orderItems.length === 0 ? (
-                <Messsage>Order is empty</Messsage>
-            ) : ( 
-                <div className="bg-white">
-      {/* Background color split screen for large screens */}
-      <div className="fixed left-0 top-0 hidden w-1/2 bg-white lg:block" aria-hidden="true" />
-      <div className="fixed right-0 top-16 hidden h-full w-1/2 bg-sky-700 lg:block" aria-hidden="true" />
-
-      <header className="relative mx-auto max-w-7xl py-4 lg:grid lg:grid-cols-2 lg:gap-x-16 lg:bg-transparent lg:px-8 lg:pb-10 lg:pt-16">
-        <div className="mx-auto flex max-w-2xl px-4 lg:w-full lg:max-w-lg lg:px-0">
-            <span className="sr-only">ELMNTM</span>
-            <img
-            src={logo}
-              alt="logo"
-              className="hidden h-10 w-auto lg:block"
+          <Messsage>Order is empty</Messsage>
+        ) : (
+          <div className="bg-white">
+            <div
+              className="fixed left-0 top-0 hidden w-1/2 bg-white lg:block"
+              aria-hidden="true"
             />
-        </div>
-      </header>
+            <div
+              className="fixed right-0 top-16 hidden h-full w-1/2 bg-sky-700 lg:block"
+              aria-hidden="true"
+            />
 
-      <main className="relative mx-auto grid max-w-7xl grid-cols-1 gap-x-16 lg:grid-cols-2 lg:px-8">
-        <section
-          aria-labelledby="summary-heading"
-          className="bg-sky-700 pb-12 rounded-md text-white md:px-10 lg:col-start-2 lg:row-start-1 lg:mx-auto lg:w-full lg:max-w-lg lg:bg-transparent lg:px-0 lg:pb-24 lg:pt-0"
-        >
-          <div className="mx-auto max-w-2xl px-4 lg:max-w-none lg:px-0">
-            <h2 id="summary-heading" className="sr-only">
-              Order summary
-            </h2>
+            <header className="relative mx-auto max-w-7xl py-4 lg:grid lg:grid-cols-2 lg:gap-x-16 lg:bg-transparent lg:px-8 lg:pb-10 lg:pt-16">
+              <div className="mx-auto flex max-w-2xl px-4 lg:w-full lg:max-w-lg lg:px-0">
+                <span className="sr-only">ELMNTM</span>
+                <img
+                  src={logo}
+                  alt="logo"
+                  className="hidden h-10 w-auto lg:block"
+                />
+              </div>
+            </header>
 
-            <dl>
-              <dd className="mt-1 text-3xl font-bold tracking-tight text-white"></dd>
-            </dl>
+            <main className="relative mx-auto grid max-w-7xl grid-cols-1 gap-x-16 lg:grid-cols-2 lg:px-8">
+              <section
+                aria-labelledby="summary-heading"
+                className="bg-sky-700 pb-12 rounded-md text-white md:px-10 lg:col-start-2 lg:row-start-1 lg:mx-auto lg:w-full lg:max-w-lg lg:bg-transparent lg:px-0 lg:pb-24 lg:pt-0"
+              >
+                <div className="mx-auto max-w-2xl px-4 lg:max-w-none lg:px-0">
+                  <h2 id="summary-heading" className="sr-only">
+                    Order summary
+                  </h2>
 
-            <ul role="list" className="divide-y divide-white divide-opacity-10 text-sm font-medium">
-              {order.orderItems.map((item, index) => (
-                <li key={index} className="flex items-start space-x-4 py-6">
-                  <img
-                    src={item.image}
-                    alt={item.name}
-                    className="h-20 w-20 flex-none rounded-md object-cover object-center"
-                  />
-                  <div className="flex-auto space-y-1">
-                    <h3 className="text-white">{item.name}</h3>
-                    <p>Qauntity: {item.qty}</p>
-                    <p>${item.price}</p>
+                  <dl>
+                    <dd className="mt-1 text-3xl font-bold tracking-tight text-white"></dd>
+                  </dl>
+
+                  <ul
+                    role="list"
+                    className="divide-y divide-white divide-opacity-10 text-sm font-medium"
+                  >
+                    {order.orderItems.map((item, index) => (
+                      <li
+                        key={index}
+                        className="flex items-start space-x-4 py-6"
+                      >
+                        <img
+                          src={item.image}
+                          alt={item.name}
+                          className="h-20 w-20 flex-none rounded-md object-cover object-center"
+                        />
+                        <div className="flex-auto space-y-1">
+                          <h3 className="text-white">{item.name}</h3>
+                          <p>Qauntity: {item.qty}</p>
+                          <p>${item.price}</p>
+                        </div>
+                        <p className="flex-none text-base font-medium text-white">
+                          $ {(item.qty * item.price).toFixed(2)}
+                        </p>
+                      </li>
+                    ))}
+                  </ul>
+
+                  <dl className="space-y-6 border-t border-white border-opacity-10 pt-6 text-sm font-medium">
+                    <div className="flex items-center justify-between">
+                      <dt>Subtotal</dt>
+                      <dd>$ {order.itemsPrice.toFixed(2)}</dd>
+                    </div>
+
+                    <div className="flex items-center justify-between">
+                      <dt>Shipping</dt>
+                      <dd>$ {order.shippingPrice.toFixed(2)}</dd>
+                    </div>
+
+                    <div className="flex items-center justify-between">
+                      <dt>Taxes</dt>
+                      <dd>$ {order.taxPrice.toFixed(2)}</dd>
+                    </div>
+
+                    <div className="flex items-center justify-between border-t border-white border-opacity-10 pt-6 text-white">
+                      <dt className="text-base">Total</dt>
+                      <dd className="text-base">
+                        $ {order.totalPrice.toFixed(2)}
+                      </dd>
+                    </div>
+                  </dl>
+                </div>
+              </section>
+
+              <section
+                aria-labelledby="payment-and-shipping-heading"
+                className="py-16 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:w-full lg:max-w-lg lg:pb-24 lg:pt-0"
+              >
+                <div className="mx-auto max-w-2xl px-4 lg:max-w-none lg:px-0">
+                  <div>
+                    <h3
+                      id="OrderID"
+                      className="text-xl font-semibold text-gray-900"
+                    >
+                      Order ID: {order._id}
+                    </h3>
+
+                    <h3
+                      id="contact-info-heading"
+                      className="text-xl mt-5 font-medium text-gray-900"
+                    >
+                      Customer Details
+                    </h3>
+                    <div className="mt-2">
+                      <div>
+                        <p className="block text-md font-lg text-gray-800">
+                          <strong>Name:</strong>
+                          <span> {order.user.username}</span>
+                        </p>
+                      </div>
+                      <div className="mt-2">
+                        <p className="block text-md font-lg text-gray-800">
+                          <strong>Email address:</strong>
+                          <span> {order.user.email}</span>
+                        </p>
+                      </div>
+                      <div className="mt-2">
+                        <p className="block text-md font-lg text-gray-800">
+                          <strong>Address:</strong>
+                          <span>
+                            {" "}
+                            {order.shippingAddress.address},{" "}
+                            {order.shippingAddress.city}{" "}
+                            {order.shippingAddress.postalCode},{" "}
+                            {order.shippingAddress.state},{" "}
+                            {order.shippingAddress.country}
+                          </span>
+                        </p>
+                      </div>
+                    </div>
                   </div>
-                  <p className="flex-none text-base font-medium text-white">$ {(item.qty * item.price).toFixed(2)}</p>
-                </li>
-              ))}
-            </ul>
 
-            <dl className="space-y-6 border-t border-white border-opacity-10 pt-6 text-sm font-medium">
-              <div className="flex items-center justify-between">
-                <dt>Subtotal</dt>
-                <dd>$ {(order.itemsPrice).toFixed(2)}</dd>
-              </div>
+                  <div className="mt-10">
+                    <h3
+                      id="PaymentMethod"
+                      className="text-lg font-medium text-gray-800"
+                    >
+                      Payment Status
+                    </h3>
+                    {order.isPaid ? (
+                      <Message variant="success">
+                        Paid on {order.paidAt}
+                      </Message>
+                    ) : (
+                      <Message variant="danger">Unpaid</Message>
+                    )}
+                  </div>
 
-              <div className="flex items-center justify-between">
-                <dt>Shipping</dt>
-                <dd>$ {(order.shippingPrice).toFixed(2)}</dd>
-              </div>
-
-              <div className="flex items-center justify-between">
-                <dt>Taxes</dt>
-                <dd>$ {(order.taxPrice).toFixed(2)}</dd>
-              </div>
-
-              <div className="flex items-center justify-between border-t border-white border-opacity-10 pt-6 text-white">
-                <dt className="text-base">Total</dt>
-                <dd className="text-base">$ {(order.totalPrice).toFixed(2)}</dd>
-              </div>
-            </dl>
+                  <div className="mt-10">
+                    <h3 className="text-lg font-medium text-gray-900">
+                      Payment Options
+                    </h3>
+                  </div>
+                  <div className="mt-5 flex justify-center border-t border-gray-200 pt-6">
+                    {!order.isPaid && (
+                      <div>
+                        {loadingPay && <Loader />}
+                        {isPending ? <Loader /> : <div>
+                            <PayPalButtons className=""/>
+                            </div>}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </section>
+            </main>
           </div>
-        </section>
-
-        <section
-          aria-labelledby="payment-and-shipping-heading"
-          className="py-16 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:w-full lg:max-w-lg lg:pb-24 lg:pt-0"
-        >
-            <div className="mx-auto max-w-2xl px-4 lg:max-w-none lg:px-0">
-              <div>
-                <h3 id="contact-info-heading" className="text-xl font-semibold text-gray-900">
-                  Order ID: {order._id}
-                </h3>
-
-                <div className="mt-5">
-                <div>
-                  <p className="block text-md font-lg text-gray-800">
-                    <strong>Name:</strong><span> {order.user.username}</span>
-                  </p>
-                  </div>
-                  <div className="mt-2">
-                  <p className="block text-md font-lg text-gray-800">
-                    <strong>Email address:</strong><span> {order.user.email}</span>
-                  </p>
-                  </div>
-                  <div className="mt-2">
-                  <p className="block text-md font-lg text-gray-800">
-                    <strong>Address:</strong><span> {order.shippingAddress.address}, {order.shippingAddress.city} {order.shippingAddress.postalCode}, {order.shippingAddress.state}, {order.shippingAddress.country}</span>
-                  </p>
-                  </div>
-                </div>
-              </div>
-
-              <div className="mt-10">
-                <h3 id="shipping-heading" className="text-lg font-medium text-gray-900">
-                  Shipping address
-                </h3>
-
-                
-              </div>
-
-              <div className="mt-10">
-                <h3 className="text-lg font-medium text-gray-900">Billing information</h3>
-
-                <div className="mt-6 flex items-center">
-                  <input
-                    id="same-as-shipping"
-                    name="same-as-shipping"
-                    type="checkbox"
-                    defaultChecked
-                    className="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                  />
-                  <div className="ml-2">
-                    <label htmlFor="same-as-shipping" className="text-sm font-medium text-gray-900">
-                      Same as shipping information
-                    </label>
-                  </div>
-                </div>
-              </div>
-
-              <div className="mt-10 flex justify-end border-t border-gray-200 pt-6">
-                <button
-                  type="submit"
-                  className="rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
-                >
-                  Pay now
-                </button>
-              </div>
-            </div>
-        </section>
-      </main>
+        )}
+      </div>
     </div>
-            )}
-    </div>
-    </div> 
   );
 };
 
