4a975e251d586d113dc052d3abe17e41ac2d4dc7
Niko Auer <nikoauer26@gmail.com>
2024-02-18 12:22:13 +1100

    added some modifications to profile button for seeing orders

--- Frontend/src/pages/User/Profile.jsx
+++ Frontend/src/pages/User/Profile.jsx
@@ -6,7 +6,13 @@ import { useDispatch, useSelector } from "react-redux"
 import { NavLink } from "react-router-dom"
 import { useProfileMutation } from "../../redux/api/usersAPISlice"
 import logo from "../../images/logo8.svg"
-import { ChevronRightIcon } from "@heroicons/react/24/outline";
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+import { ChevronDownIcon } from '@heroicons/react/20/solid'
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
 
 
 const Profile = () => {
@@ -42,32 +48,62 @@ const Profile = () => {
         }
       };
 
-      const pages = [
-        { name: 'Update Profile', to:"/profile", current: false },
-        { name: 'My Orders', to: '/user-orders', current: true },
-      ]
-
     return(
         <>
-        {/* Navigation for profile and orders */}
-        <nav className="flex justify-center" aria-label="Breadcrumb">
-        <ol role="list" className="flex items-center space-x-4">
-          {pages.map((page) => (
-            <li key={page.name}>
-              <div className="flex items-center">
-                <ChevronRightIcon className="h-5 w-5"></ChevronRightIcon>
-                <NavLink
-                  to={page.to}
-                  className="ml-4 text-sm font-medium text-blue-500 hover:text-blue-700"
-                  aria-current={page.current ? 'page' : undefined}
-                >
-                  {page.name}
-                </NavLink>
+          <div className="flex items-center justify-center mt-1">
+            <div className="px-4 sm:px-6 lg:px-8 w-full max-w-screen-xl">
+              <div className="flex justify-end">
+              <Menu as="div" className="relative inline-block justify-end text-left">
+                  <div>
+                    <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                      Options
+                      <ChevronDownIcon className="-mr-1 h-5 w-5 text-gray-400" aria-hidden="true" />
+                    </Menu.Button>
+                  </div>
+
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <div className="py-1">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <NavLink
+                              to='/profile'
+                              className={classNames(
+                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                'block px-4 py-2 text-sm'
+                              )}
+                            >
+                              Update Profile
+                            </NavLink>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <NavLink
+                              to='/user-orders'
+                              className={classNames(
+                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                'block px-4 py-2 text-sm'
+                              )}
+                            >
+                              My Orders
+                            </NavLink>
+                          )}
+                        </Menu.Item>
+                      </div>
+                    </Menu.Items>
+                  </Transition>
+            </Menu>
               </div>
-            </li>
-          ))}
-        </ol>
-      </nav>
+
       {/* update profile section */}
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -173,6 +209,8 @@ const Profile = () => {
             {loadingUpdateProfile && <Loader />}
           </form>
         </div>
+      </div>
+      </div>
       </div>
       </>
     )
