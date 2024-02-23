5ae5c3b78a5c7a3a191d6282b7c4f359c49e00ed
Niko Auer <nikoauer26@gmail.com>
2024-02-23 12:44:51 +1100

    modified home banner styling

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -3,9 +3,11 @@ import { useGetProductsQuery } from "../redux/api/productAPISlice";
 import Message from "../components/Message";
 import Loader from "../components/Loader";
 import Carousel from "../components/Carousel";
-import Banner from "../images/Banner.png";
+import Banner from "../images/Homebanner.png";
 import Product from "./Products/Product";
 import { FaArrowRight } from "react-icons/fa";
+
+
 
 const Home = () => {
   const { keyword } = useParams();
@@ -20,18 +22,16 @@ const Home = () => {
         </Message>
       ) : (
         <>
-            <div className="relative">
-              <img src={Banner} alt="Be in your element" className="min-w-full" />
-              <div className="absolute xl:left-10 md:left-10 sm:left-5 top-1/2 transform -translate-y-1/2 sm:w-1/3  md:w-1/3 lg:w-1/3">
-                <h1 className="text-white xl:text-8xl lg:text-5xl md:text-4xl sm:text-3xl font-bold mb-2 sm:py-2">
-                  Don't let the elements stop the adventure
-                </h1>
-                  <Link to="/shop" className="inline-flex items-center bg-sky-500 hover:bg-sky-600 text-black hover:text-white px-2 sm:px-4 py-1 sm:py-2 rounded text-sm sm:text-base">
-                    <span>Shop the latest gear</span>
-                    <FaArrowRight className="ml-2" />
-                  </Link>
-              </div>
-            </div>
+<div className="relative">
+  <img src={Banner} alt="Be in your element" className="min-w-full" />
+  <div className="absolute inset-0 flex justify-center items-center mt-10">
+    <button className="bg-sky-600 hover:bg-sky-700 text-white font-bold py-2 px-5 rounded-md">
+      <Link to='/shop'>Shop Now</Link>
+    </button>
+  </div>
+</div>
+
+
 
           <div className="flex items-center justify-center mt-1">
             <div className="px-4 sm:px-6 lg:px-8 w-full max-w-screen-xl">
