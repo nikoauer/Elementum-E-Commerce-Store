44b25c291a472b31562f9aea7ee975712c45aefc
Niko Auer <nikoauer26@gmail.com>
2024-02-24 10:46:16 +1100

    mdoified config file fand removed useNewURLParser and useZUnifiedTopology

--- Backend/config/connection.js
+++ Backend/config/connection.js
@@ -1,15 +1,10 @@
 import mongoose from 'mongoose';
 
-const { connect, connection } = mongoose;
-
 const connectDB = async () => {
   try {
     const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/Elementum';
 
-    await connect(connectionString, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(connectionString);
 
     console.log('MongoDB Connected...');
   } catch (error) {
