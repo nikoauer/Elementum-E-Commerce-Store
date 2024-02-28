2e07b96bbc2f468f95a2bf6c0261537553ce4da1
Niko Auer <nikoauer26@gmail.com>
2024-02-28 18:10:30 +1100

    changed loader to see if it fixed deploy bug

--- Frontend/src/components/loader.jsx
+++ Frontend/src/components/loader.jsx
@@ -3,11 +3,7 @@ import React from "react";
 const Loader = () => {
   return (
     <div className="flex justify-center">
-      <div
-        className="animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-blue-600 rounded-full dark:text-blue-500"
-        role="status"
-        aria-label="loading"
-      >
+      <div className="border-gray-300 h-10 w-10 animate-spin rounded-full border-8 border-t-blue-600">
         <span className="sr-only">Loading...</span>
       </div>
     </div>
