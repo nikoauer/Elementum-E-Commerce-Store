5840502e9165bb5c5ce9ba8a2b7ce5ad3bd1fd6c
Niko Auer <nikoauer26@gmail.com>
2024-03-01 09:52:38 +1100

    Revert "mdoified config file fand removed useNewURLParser and useZUnifiedTopology"
    This reverts commit 44b25c291a472b31562f9aea7ee975712c45aefc.

--- Backend/config/connection.js
+++ Backend/config/connection.js
@@ -1,10 +1,15 @@
 import mongoose from 'mongoose';
 
+const { connect, connection } = mongoose;
+
 const connectDB = async () => {
   try {
     const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/Elementum';
 
-    await mongoose.connect(connectionString);
+    await connect(connectionString, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
     console.log('MongoDB Connected...');
   } catch (error) {
